1caa2da7742d41b1e40f0797621a935dea73f060
Rutte Oliveira <rosrosrutte@gmail.com>
2022-04-08 22:33:00 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double milhas, km;
+
+Console.Write ("Entre com a medida (em milhas): ");
+milhas = Convert.ToDouble(Console.ReadLine());
+
+km = milhas * 1.609;
+
+Console.WriteLine($"{km:N3} Km");
